f068584698b696522030850cfdd067fad42cf42f
wangqirun <wqr180322@163.com>
2020-10-22 14:17:59 +0100

    new group

--- group.py
+++ group.py
@@ -2,4 +2,10 @@
 
 # Your code to go here...
 
-my_group =
+my_group = {'Jill':{'Age':26,'Job':'biologist','Relationship':{'Zalika':'friend','John':'partner'}},
+'Zalika':{'Age':28,'Job':'artist','Relationship':{'Jill':'friend'}},
+'John':{'Age':27,'Job':'writer','Relationship':{'Jill':'partner'}},
+'Nash':{'Age':34,'Job':'chef','Relationship':{'John':'cousin','Zalika':'landlord'}}}
+
+
+
